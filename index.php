b267ccb359cec5664fcfaa76e9352305331ca971
amagami-youtube <129277952+amagami-youtube@users.noreply.github.com>
2023-04-03 16:53:47 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,129 @@
+<?php
+
+// データベースの接続情報
+define( 'DB_HOST', 'localhost');
+define( 'DB_USER', 'root');
+define( 'DB_PASS', 'password');
+define( 'DB_NAME', 'board');
+
+// タイムゾーン設定
+date_default_timezone_set('Asia/Tokyo');
+
+// 変数の初期化
+$current_date = null;
+$message = array();
+$message_array = array();
+$error_message = array();
+$pdo = null;
+$stmt = null;
+$res = null;
+$option = null;
+
+session_start();
+
+// データベースに接続
+try {
+
+    $option = array(
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::MYSQL_ATTR_MULTI_STATEMENTS => false
+    );
+    $pdo = new PDO('mysql:charset=UTF8;dbname='.DB_NAME.';host='.DB_HOST , DB_USER, DB_PASS, $option);
+
+} catch(PDOException $e) {
+
+    // 接続エラーのときエラー内容を取得する
+    $error_message[] = $e->getMessage();
+}
+
+if( !empty($_POST['btn_submit']) ) {
+
+    // 空白除去
+	$view_name = preg_replace( '/\A[\p{C}\p{Z}]++|[\p{C}\p{Z}]++\z/u', '', $_POST['view_name']);
+	$message = preg_replace( '/\A[\p{C}\p{Z}]++|[\p{C}\p{Z}]++\z/u', '', $_POST['message']);
+
+	// 表示名の入力チェック
+	if( empty($view_name) ) {
+		$error_message[] = '表示名を入力してください。';
+	} else {
+
+        // セッションに表示名を保存
+		$_SESSION['view_name'] = $view_name;
+    }
+
+	// メッセージの入力チェック
+	if( empty($message) ) {
+		$error_message[] = 'ひと言メッセージを入力してください。';
+	} else {
+
+        // 文字数を確認
+        if( 100 < mb_strlen($message, 'UTF-8') ) {
+			$error_message[] = 'ひと言メッセージは100文字以内で入力してください。';
+		}
+    }
+
+	if( empty($error_message) ) {
+		
+		// 書き込み日時を取得
+        $current_date = date("Y-m-d H:i:s");
+
+        // トランザクション開始
+        $pdo->beginTransaction();
+
+        try {
+
+            // SQL作成
+            $stmt = $pdo->prepare("INSERT INTO message (view_name, message, post_date) VALUES ( :view_name, :message, :current_date)");
+
+            // 値をセット
+            $stmt->bindParam( ':view_name', $view_name, PDO::PARAM_STR);
+            $stmt->bindParam( ':message', $message, PDO::PARAM_STR);
+            $stmt->bindParam( ':current_date', $current_date, PDO::PARAM_STR);
+
+            // SQLクエリの実行
+            $res = $stmt->execute();
+
+            // コミット
+            $res = $pdo->commit();
+
+        } catch(Exception $e) {
+
+            // エラーが発生した時はロールバック
+            $pdo->rollBack();
+        }
+
+        if( $res ) {
+            $_SESSION['success_message'] = 'メッセージを書き込みました。';
+        } else {
+            $error_message[] = '書き込みに失敗しました。';
+        }
+
+        // プリペアドステートメントを削除
+        $stmt = null;
+
+        header('Location: ./');
+        exit;
+	}
+}
+
+if( !empty($pdo) ) {
+
+    // メッセージのデータを取得する
+    $sql = "SELECT view_name,message,post_date FROM message ORDER BY post_date DESC";
+    $message_array = $pdo->query($sql);
+}
+
+// データベースの接続を閉じる
+$pdo = null;
+
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
 <meta charset="utf-8">
 <title>ひと言掲示板</title>
 <style>
+
 /*------------------------------
  Reset Style
  
@@ -28,24 +148,30 @@ time, mark, audio, video {
     vertical-align:baseline;
     background:transparent;
 }
+
 body {
     line-height:1;
 }
+
 article,aside,details,figcaption,figure,
 footer,header,hgroup,menu,nav,section {
     display:block;
 }
+
 nav ul {
     list-style:none;
 }
+
 blockquote, q {
     quotes:none;
 }
+
 blockquote:before, blockquote:after,
 q:before, q:after {
     content:'';
     content:none;
 }
+
 a {
     margin:0;
     padding:0;
@@ -53,12 +179,14 @@ a {
     vertical-align:baseline;
     background:transparent;
 }
+
 /* change colours to suit your needs */
 ins {
     background-color:#ff9;
     color:#000;
     text-decoration:none;
 }
+
 /* change colours to suit your needs */
 mark {
     background-color:#ff9;
@@ -66,17 +194,21 @@ mark {
     font-style:italic;
     font-weight:bold;
 }
+
 del {
     text-decoration: line-through;
 }
+
 abbr[title], dfn[title] {
     border-bottom:1px dotted;
     cursor:help;
 }
+
 table {
     border-collapse:collapse;
     border-spacing:0;
 }
+
 hr {
     display:block;
     height:1px;
@@ -85,9 +217,11 @@ hr {
     margin:1em 0;
     padding:0;
 }
+
 input, select {
     vertical-align:middle;
 }
+
 /*------------------------------
 Common Style
 ------------------------------*/
@@ -98,27 +232,42 @@ body {
 	color: #222;
 	background: #f7f7f7;
 }
+
 a {
     color: #007edf;
     text-decoration: none;
 }
+
 a:hover {
     text-decoration: underline;
 }
+
+.wrapper {
+    display: flex;
+    margin: 0 auto 50px;
+    padding: 0 20px;
+    max-width: 1200px;
+    align-items: flex-start;
+}
+
 h1 {
 	margin-bottom: 30px;
     font-size: 100%;
     color: #222;
     text-align: center;
 }
+
+
 /*-----------------------------------
 入力エリア
 -----------------------------------*/
+
 label {
     display: block;
     margin-bottom: 7px;
     font-size: 86%;
 }
+
 input[type="text"],
 textarea {
 	margin-bottom: 20px;
@@ -128,6 +277,7 @@ textarea {
     border-radius: 3px;
     background: #fff;
 }
+
 input[type="text"] {
 	width: 200px;
 }
@@ -152,10 +302,12 @@ input[type=submit]:hover,
 button:hover {
     background-color: #2392d8;
 }
+
 hr {
 	margin: 20px 0;
 	padding: 0;
 }
+
 .success_message {
     margin-bottom: 20px;
     padding: 10px;
@@ -163,6 +315,7 @@ hr {
     border-radius: 10px;
     border: 1px solid #4dc100;
 }
+
 .error_message {
     margin-bottom: 20px;
     padding: 10px;
@@ -171,14 +324,18 @@ hr {
     border-radius: 10px;
     border: 1px solid #ff5f79;
 }
+
 .success_message,
 .error_message li {
     font-size: 86%;
     line-height: 1.6em;
 }
+
+
 /*-----------------------------------
 掲示板エリア
 -----------------------------------*/
+
 article {
 	margin-top: 20px;
 	padding: 20px;
@@ -221,10 +378,13 @@ article.reply::before {
         font-size: 86%;
         line-height: 1.6em;
     }
+
 @media only screen and (max-width: 1000px) {
+
     body {
         padding: 30px 5%;
     }
+
     input[type="text"] {
         width: 100%;
     }
@@ -238,10 +398,41 @@ article.reply::before {
 </head>
 <body>
 <h1>ひと言掲示板</h1>
-<!-- ここにメッセージの入力フォームを設置 -->
+<?php if( empty($_POST['btn_submit']) && !empty($_SESSION['success_message']) ): ?>
+    <p class="success_message"><?php echo htmlspecialchars( $_SESSION['success_message'], ENT_QUOTES, 'UTF-8'); ?></p>
+    <?php unset($_SESSION['success_message']); ?>
+<?php endif; ?>
+<?php if( !empty($error_message) ): ?>
+    <ul class="error_message">
+		<?php foreach( $error_message as $value ): ?>
+            <li>・<?php echo $value; ?></li>
+		<?php endforeach; ?>
+    </ul>
+<?php endif; ?>
+<form method="post">
+	<div>
+		<label for="view_name">表示名</label>
+		<input id="view_name" type="text" name="view_name" value="<?php if( !empty($_SESSION['view_name']) ){ echo htmlspecialchars( $_SESSION['view_name'], ENT_QUOTES, 'UTF-8'); } ?>">
+	</div>
+	<div>
+		<label for="message">ひと言メッセージ</label>
+		<textarea id="message" name="message"><?php if( !empty($message) ){ echo htmlspecialchars( $message, ENT_QUOTES, 'UTF-8'); } ?></textarea>
+	</div>
+	<input type="submit" name="btn_submit" value="書き込む">
+</form>
 <hr>
 <section>
-<!-- ここに投稿されたメッセージを表示 -->
+<?php if( !empty($message_array) ){ ?>
+<?php foreach( $message_array as $value ){ ?>
+<article>
+    <div class="info">
+        <h2><?php echo htmlspecialchars( $value['view_name'], ENT_QUOTES, 'UTF-8'); ?></h2>
+        <time><?php echo date('Y年m月d日 H:i', strtotime($value['post_date'])); ?></time>
+    </div>
+    <p><?php echo nl2br( htmlspecialchars( $value['message'], ENT_QUOTES, 'UTF-8') ); ?></p>
+</article>
+<?php } ?>
+<?php } ?>
 </section>
 </body>
 </html>
